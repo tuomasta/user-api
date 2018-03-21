5bb44421ee951965e0697559fc7b0e089fa82f41
tuomasta <my@email.com>
2018-03-21 17:29:21 +0200

    starting to work with authentication api

---
+++ AuthenticationApi/AuthenticationWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthenticationWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
