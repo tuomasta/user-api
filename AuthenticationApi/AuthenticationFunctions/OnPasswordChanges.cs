e055925f6c0b359f1cab94e0042ae68776f8d9d6
Tuomas Talus <talus.tuomas@gmail.com>
2018-03-24 12:05:38 +0200

    clean up

--- AuthenticationApi/AuthenticationFunctions/OnPasswordChanges.cs
+++ AuthenticationApi/AuthenticationFunctions/OnPasswordChanges.cs
@@ -2,10 +2,7 @@ using DataAccess;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
-using System;
 using System.Configuration;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace OnPasswordChanges
 {
@@ -17,18 +14,15 @@ namespace OnPasswordChanges
             var authInfo = JsonConvert.DeserializeObject<AuthentincationInfo>(passwordChangeJson);
             log.Info($"C# Queue trigger function processed: {passwordChangeJson}");
 
-            // store only a has of the password
+            // store only the hash of the password
             authInfo.Password = authInfo.Password.ToSHA256Hash();
 
             var repo = new AuthRepo(ConnectionString, DatabaseName, CollectionName);
             repo.CreateAuthentication(authInfo);
         }
 
-
-
-        static public string ConnectionString => ConfigurationManager.AppSettings.Get("AuthDbConnection");
-
-        public static string DatabaseName => ConfigurationManager.AppSettings.Get("AuthDb"); // Environment.GetEnvironmentVariable("AuthDb", EnvironmentVariableTarget.Process);
+        public static string ConnectionString => ConfigurationManager.AppSettings.Get("AuthDbConnection");
+        public static string DatabaseName => ConfigurationManager.AppSettings.Get("AuthDb");
         public static string CollectionName => ConfigurationManager.AppSettings.Get("AuthCollection");
     }
 }
--- AuthenticationApi/DataAccessTests/SHA256HashTests.cs
+++ AuthenticationApi/DataAccessTests/SHA256HashTests.cs
@@ -5,14 +5,13 @@ using FluentAssertions;
 namespace DataAccessTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ToSHA256Hash
     {
         [TestMethod]
         public void WHEN_calculating_SHA256_THEN_returns_correct_hash_string()
         {
             var hash = "foobar".ToSHA256Hash();
             hash.Should().Be("c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2");
-
         }
     }
 }
