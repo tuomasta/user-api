026527a3c1b53d5194ef1d68022e1f285d4e5d05
Tuomas Talus <talus.tuomas@gmail.com>
2018-04-02 22:58:06 +0300

    Fixed bug when reading allowed domains from appsettings.json

--- AuthenticationApi/AuthenticationApi/Startup.cs
+++ AuthenticationApi/AuthenticationApi/Startup.cs
@@ -24,7 +24,7 @@ namespace AuthenticationApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var domains = Configuration.GetValue<string[]>("Cors:Domains");
+            var domains = Configuration.GetSection("Cors:Domains").GetChildren().Select(c => c.Value).ToArray();
             if (domains?.Any() == true) {
                 services.AddCors(options => 
                     options.AddPolicy("default", policy => policy.WithMethods("POST,DELETE").WithOrigins(domains).AllowAnyHeader()));
